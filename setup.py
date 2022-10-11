822657ec032472bc25d25c4e10923879a5a8259a
shinilseop <shinilseop12@gmail.com>
2022-10-12 01:15:29 +0900

    add @

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='detectron2dependencies',
     packages=find_packages(),
-    url='https://github.abc.com/abc/myabc',
+    url='https://github.com/shinilseop/detectron2dependencies',
     description='detectron2 dependencies',
-    long_description=open('README.md').read(),
     install_requires=[
         "torch==1.11.0",
         "torchvision==0.12.0",
         "torchaudio==0.11.0",
         "pyyaml==5.1",
         "opencv-python==4.6.0.66",
-        "git+https://github.com/facebookresearch/detectron2.git"
+        "detectron2 @ git+https://github.com/facebookresearch/detectron2.git"
     ],
     dependency_links=[
         "git+https://github.com/facebookresearch/detectron2.git",
@@ -20,4 +19,3 @@ setup(
     ],
     include_package_data=True,
 )
-
